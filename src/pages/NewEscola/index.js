8ba63481728864417831a2ef1abf2ebba83e9a3a
matheus.silva <tecmatheusg@hotmail.com>
2023-03-01 11:13:48 -0300

    newEscola visual alterado

--- src/pages/NewEscola/index.js
+++ src/pages/NewEscola/index.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 import api from "../../services/api";
-import "./styles.css";
 import logoImage from "../../assets/logo.svg";
 
 export default function NewEscola() {
@@ -38,7 +37,7 @@ export default function NewEscola() {
   async function saveOrUpdate(e) {
     e.preventDefault();
 
-    const data = {    
+    const data = {
       nome,
       endereco,
     };
@@ -57,32 +56,37 @@ export default function NewEscola() {
   }
 
   return (
-    <div className="new-escola-container">
-      <div className="content">
-        <section className="form">
-          <img src={logoImage} alt="logo" />
-          <h1>Cadastrar Escola</h1>
-          <p>Coloque as informações da escola e clique em 'Cadastrar'</p>
-          <Link className="back-link" to="/escolas">
-            <button>Voltar</button>
-          </Link>
-        </section>
-        <form onSubmit={saveOrUpdate}>
-          <input
-            placeholder="Nome"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-          />
-          <input
-            placeholder="Endereço"
-            value={endereco}
-            onChange={(e) => setEndereco(e.target.value)}
-          />
-          <button className="button" type="submit">
-            Cadastrar / Alterar
-          </button>
-        </form>
+    <>
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col">
+            <img src={logoImage} style={{ height: "300px" }} alt="logo" />
+            <h1>Cadastrar Escola</h1>
+            <p>Coloque as informações da escola e clique em 'Cadastrar'</p>
+            <Link className="back-link" to="/escolas">
+              <button className="btn btn-primary btn-lg" type="button">Voltar</button>
+            </Link>
+          </div>
+          <div className="col">
+            <form onSubmit={saveOrUpdate} className="mt-5">
+              <div className="input-group input-group-lg mb-3">
+                <input type="text" placeholder="Nome"
+                  value={nome}
+                  onChange={(e) => setNome(e.target.value)}
+                  className="form-control" />
+              </div>
+              <div className="input-group input-group-lg mb-3">
+                <input type="text" placeholder="Endereço" value={endereco}
+                  onChange={(e) => setEndereco(e.target.value)}
+                  className="form-control" />
+              </div>
+              <button className="btn btn-primary btn-lg w-100" type="submit">
+                Cadastrar / Alterar
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
